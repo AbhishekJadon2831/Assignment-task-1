89c05656db19b6291195172b2c9fa4d2b23a96d8
abhishekjadon2831 <abhishekjadon616@gmail.com>
2025-01-21 21:50:02 +0530

    abhishek

---
+++ index1.js
@@ -0,0 +1,122 @@
+// Q=1
+
+// let score = 65;
+// if (score >= 90) {
+//     console.log("greade A");
+
+// } else if (score >= 80 && score < 89) {
+//     console.log("greade B");
+
+// } else if (score >= 70 && score <= 79) {
+//     console.log("greade c");
+
+// } else if (score < 70 && score >= 50) {
+//     console.log("greade d");
+
+// } else {
+//     console.log("fail");
+
+// }
+
+
+
+
+
+//Q=2
+
+//  let a=5;
+//  let b=5;
+//  let c=7;
+//  if(a === b && b === c && a === c){
+//      console.log("Equilateral triangle.");
+
+//  }else if(a === b || b === c || a===c){
+//      console.log("Isosceles triangle.");
+
+//  }else{
+//      console.log("Scalene triangle");
+//     
+//  }
+
+
+
+
+
+// Q=3
+
+// let num=30;
+// if(num % 2===0){
+//     if(num>50){
+//         console.log("Even and Large");
+
+//     }else{
+//         console.log("Even and small");
+
+//     }
+// }else{
+//     if(num<30){
+//         console.log("Odd and small");
+
+//     }else{
+//         console.log("Odd and Large");
+
+//     }
+// }
+
+
+
+// Q=1
+
+// let a=5;
+// for(let i=1; i<=10; i++){
+//     console.log(i*a);
+
+// }
+
+
+
+
+// Q=2
+
+// let num=6;
+
+// let tamp=0;
+// for(let i=1; i<=num; i++){
+//     tamp+=i;
+// }
+// console.log(tamp);
+
+
+
+//Q=3
+
+
+// const rows = 4;
+// let a = '';
+// for (let i = 1; i <= rows; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     a += j + ' ';
+//   }
+//   a += '\n';
+// }
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
