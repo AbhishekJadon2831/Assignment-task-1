697ee2b963dcb23f46f09c09cb9f82416484f9b4
AbhishekJadon2831 <abhishekjadon616@gmail.com>
2025-01-23 21:17:18 +0530

    For Loop Practice

--- New folder (7)/Assignment-task-1/index1.js
+++ New folder (7)/Assignment-task-1/index1.js
@@ -133,20 +133,50 @@
 
 
 
-//Q=3
+Q = 3
 
+
+// let a=6;
+// for(let i=1; i<=a; i++){
+//     space=""
+//     for(let j=a; j>=i; j--){
+//         space+=" "
+//     }
+//     for(let k=1; k<=i*2-1; k++){
+//         space+="*"
+//     }
+//     console.log(space);
+
+
+// }
 
 // let a=5;
+// for(let i=1; i<=a-1; i++){
+//     space=" "
+//     for(let j=1; j<=i; j++){
+//         space+="1"
+//     }
+//     for(k=a+2; k>=i; k--){
+//         space+="*"
+//     }
+//     console.log(space);
 
-// for (let i = 1; i <= a; i++) {
-//     let pattern = "   ";
-//     for (let j = 1; j <= a - i; j++) {
-//         pattern += " ";
+
+// }
+
+
+
+// for(let i=1; i<=a; i++){
+//     space=" "
+//     for(let j=1; j<=i; j++){
+//         space+=" "
 //     }
-//     for (let k = 1; k <= 2 * i - 1; k++) {
-//         pattern += "*";
+//     for(k=a; k>=i*2; k--){
+//         space+="1"
 //     }
-//     console.log(pattern);
+//     console.log(space);
+
+
 // }
 
 
@@ -159,7 +189,7 @@
 //         str=str+=j+" "
 //     }
 //     console.log(str);
-    
+
 // }
 
 
@@ -173,7 +203,7 @@
 //         str=str+=j+" "
 //     }
 //     console.log(str);
-    
+
 // }
 
 
@@ -184,16 +214,186 @@
 
 //Q=6
 
-    //  let n=9;
+//  let n=9;
 
-    // for (let i = 1; i <= n; i += 2) {
-    //     console.log(" ".repeat((n - i) / 2) + "*".repeat(i));
-    // }
+// for (let i = 1; i <= n; i += 2) {
+//     console.log(" ".repeat((n - i) / 2) + "*".repeat(i));
+// }
 
 
-    // for (let i = n - 2; i >= 1; i -= 2) {
-    //     console.log(" ".repeat((n - i) / 2) + "*".repeat(i));
-    // }
+// for (let i = n - 2; i >= 1; i -= 2) {
+//     console.log(" ".repeat((n - i) / 2) + "*".repeat(i));
+// }
+
+
+
+
+
+//     let a=6;
+//  for(let i=1; i<=a; i++){
+//     space=""
+//     for(let j=a; j>=i; j--){
+//         space+=" "
+//     }
+//     for(let k=1; k<=i*2-1; k++){
+//         space+="*"
+//     }
+//     console.log(space);
+
+
+
+// }
+
+
+// let a=6;
+// for(let i=1; i<=a; i++){
+//    space=""
+//    for(let j=a; j>=i; j--){
+//        space+=" "
+//    }
+//    for(let k=1; k<=i*2-1; k++){
+//        space+="*"
+//    }
+//    console.log(space)
+
+// }
+
+// for(let i=a-1; i>=1; i--){
+//     space=""
+//     for(let j=a; j>=i; j--){
+//         space+=" "
+//     }
+//     for(let k=1; k<=i*2-1; k++){
+//         space+="*"
+//     }
+//     console.log(space);
+
+
+// }
+
+
+
+
+
+//For Loop Practice
+
+
+
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 != 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i * i);
+// }
+
+
+// for(let i=1; i<=100; i++){
+//     if(i % 5===0){
+
+//         console.log(i);
+
+//     }
+
+// }
+
+
+
+// let a = 0;
+// let b = 1;
+
+// console.log(a); 
+// console.log(b); 
+
+// for (let i = 2; i < 10; i++) {
+//   let c = a + b;
+//   console.log(c);
+//   a = b;
+//   b = c;
+// }
+
+
+
+
+
+
+
+// for(let i=1; i<=5; i++){
+//     str=""
+//     for(let j=1; j<=i; j++){
+//         str+="*"
+//     }
+//     console.log(str);
+
+// }
+
+
+
+
+
+// for(let i=1; i<=10; i++){
+//     console.log("7", "*", i, "=", i*7);
+
+// }
+
+
+// function printCharacters(str) {
+//     for (let i = 0; i < str.length; i++) {
+//         console.log(str[i]);
+//     }
+// }
+
+
+// const myString = "Hello, world!";
+// printCharacters(myString);
+
+
+
+// let a=50;
+// let s = 0;
+// for (let i = 1; i <= a; i++) {
+//     if(i % 2==0){
+//         s += i;
+//     }
+
+// }
+// console.log(s);
+
+
+
+
+
+
+// for (let i = 1; i <= 3; i++) {
+//     let row = " ";
+//     for(let j=3; j>=i; j--){
+//         row+=" "
+//     }
+//     for (let j = 1; j <= i; j++) {
+//         row += j;
+//     }
+//     for (let j = i - 1; j >= 1; j--) {
+//         row += j
+//     }
+//     console.log(row);
+// }
+
+
+
+
 
 
 
