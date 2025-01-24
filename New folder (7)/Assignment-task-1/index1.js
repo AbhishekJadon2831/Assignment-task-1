deccec415f8808b9fd72044cf97b06548edfe48f
AbhishekJadon2831 <abhishekjadon616@gmail.com>
2025-01-24 20:54:10 +0530

    While and Switch Case Questions.

--- New folder (7)/Assignment-task-1/index1.js
+++ New folder (7)/Assignment-task-1/index1.js
@@ -312,18 +312,18 @@ Q = 3
 
 
 
-// let a = 0;
-// let b = 1;
+// let n1=0;
+// let n2=1;
 
-// console.log(a); 
-// console.log(b); 
+// for(let i=3; i<=10; i++){
+//     let next=n1+n2;
+//     n1=n2;
+//     n2=next;
+//     console.log(next);
 
-// for (let i = 2; i < 10; i++) {
-//   let c = a + b;
-//   console.log(c);
-//   a = b;
-//   b = c;
 // }
+
+
 
 
 
@@ -377,19 +377,283 @@ Q = 3
 
 
 
-// for (let i = 1; i <= 3; i++) {
-//     let row = " ";
-//     for(let j=3; j>=i; j--){
-//         row+=" "
+// let a=5;
+// for(let i=1; i<=a; i++){
+//     let  str=""
+//     for(let j=a; j>=i; j--){
+//          str+=" "
 //     }
-//     for (let j = 1; j <= i; j++) {
-//         row += j;
+//     for(let k=1; k<=i; k++){
+//         str+=k
 //     }
-//     for (let j = i - 1; j >= 1; j--) {
-//         row += j
+//     for(let l=i-1; l>=1; l--){
+//         str+=l
 //     }
-//     console.log(row);
+//     console.log(str);
+
+
+
 // }
+
+
+//While Loop Questions
+
+//Q=1
+
+// let i=1;
+// while(i<=10){
+//     console.log(i);
+//     i++
+
+// }
+
+
+
+//Q=2
+
+// let i=1;
+// let sum=0;
+// while(i<=20){
+//     if(i%2===0){
+//       sum+=i;
+//     }
+//     i++;
+// }
+// console.log(sum);
+
+
+
+//Q=3
+
+// let i=5;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+
+// }
+
+
+
+//Q=4
+
+// function isPalindrome(num) {
+//     let originalNum = num;  
+//     let reversedNum = 0;    
+
+//     while (num > 0) {
+//         let digit = num % 10;  
+//         reversedNum = reversedNum * 10 + digit;  
+//         num = Math.floor(num / 10);  
+//     }
+
+    
+//     if (originalNum === reversedNum) {
+//         return "Palindrome";  
+//     } else {
+//         return "Not a Palindrome";  
+//     }
+// }
+
+
+// let input = 121;
+// console.log(isPalindrome(input));  
+
+
+
+
+//Q=5
+
+// let i=1;
+// let fe=1;
+// while(i<=5){
+//     fe*=i;
+//     i++
+
+// }
+// console.log(fe);
+
+
+
+
+
+//Switch Case Questions
+
+//Q=1
+
+// let num1 = 5;
+// let num2 = 3;
+// let operation = "+";
+// switch (operation) {
+//     case "+":
+//         console.log(num1 + num2);
+//         break;
+
+//     case "-":
+//         console.log(num1 - num2);
+//         break;
+
+//     case "/":
+//         console.log(num1 / num2);
+//         break;
+
+//     case "*":
+//         console.log(num1 * num2);
+//         break;
+
+//     default:
+//         console.log("NAN");
+
+
+// }
+
+
+
+
+//Q=2
+
+// let day = 1;
+// switch (day) {
+//     case 1:
+//         console.log(" Monday");
+//         break;
+
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+
+//     case 4:
+//         console.log("Thursday");
+//         break;
+
+//     case 5:
+//         console.log("Friday");
+//         break;
+
+//     case 6:
+//         console.log("Saturday ");
+//         break;
+
+//     case 7:
+//         console.log("Sunday");
+//         break;
+
+//     default:
+//         console.log("Not day");
+
+
+// }
+
+
+
+
+//Q=3
+
+// let naxt="u";
+
+
+// switch (naxt) {
+//     case "a":
+//         console.log("Vowel");
+//         break;
+//     case "e":
+//         console.log("Vowel");
+//         break;
+//     case "i":
+//         console.log("Vowel");
+//         break;
+//     case "o":
+//         console.log("Vowel");
+//         break;
+//     case "u":
+//         console.log("Vowel");
+//         break;
+//     default:
+//         console.log("Consonant");
+// }
+
+
+
+
+//Q=4
+
+// let mon="A";
+
+
+// switch (mon) {
+//     case "A":
+//         console.log("Excellent");
+//         break;
+//     case "B":
+//         console.log("Good");
+//         break;
+//     case "C":
+//         console.log("Fair");
+//         break;
+//     case "D":
+//         console.log("Poor");
+//         break;
+//     case "F":
+//         console.log("Fail");
+//         break;
+//     default:
+//         console.log("Invalid Grade");
+// }
+
+
+
+
+//Q=5
+
+// let option = 2;
+
+// switch (option) {
+//     case 1:
+//         console.log(" Add two numbers");
+//         break;
+//     case 2:
+//         console.log("Subtract two numbers");
+//         break;
+//     case 3:
+//         console.log("Multiply two numbers");
+//         break;
+
+//     default:
+//         console.log("Exit");
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
