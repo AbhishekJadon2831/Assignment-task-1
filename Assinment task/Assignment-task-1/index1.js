46982225361308cd60a502bb5dd800b75e153b3b
AbhishekJadon2831 <abhishekjadon616@gmail.com>
2025-03-04 10:35:15 +0530

    4 march

--- Assinment task/Assignment-task-1/index1.js
+++ Assinment task/Assignment-task-1/index1.js
@@ -2254,43 +2254,80 @@
 //Assignment: Mastering Linked Lists
 
 
-// class node {
-//     constructor(data) {                      // Q-10
-//         this.data = data
-//         this.next = null
+// class node{
+// constructor(data){                          // Q-1
+//     this.data=data
+//     this.next=null
+// }
+// }
+
+// let a=new node(10)
+// let b=new node(20)
+// let c=new node(30)
+// let d=new node(40)
+// let e=new node(50)
+
+// a.next=b
+// b.next=c
+// c.next=d
+// d.next=e
+
+// let head=a
+// let current=head
+// let count=0
+
+// while (current!==null) {
+//     count++;
+//     current=current.next
+// }
+
+// let res=Math.floor(count/2)
+
+// current=head
+
+// for (let i = 0; i < res; i++) {
+//     current=current.next
+// }
+
+// console.log(current.data);
+
+
+
+
+
+
+
+// class node{
+//     constructor(value){                                        // Q-2
+//         this.value=value
+//         this.next=null
 //     }
 // }
 
-// let a = new node(1)
-// let b = new node(2)
-// let c = new node(3)
-// let d = new node(4)
-
-// a.next = b
-// b.next = c
-// c.next = d
-
-// let head = a
-// let current = head
+// let first=new node(1)
+// let second=new node(2)
+// let third=new node(3)
+// let four=new node(4)
+// let five=new node(5)
 
 
-// function revrselist(current) {
+// first.next=second
+// second.next=third
+// third.next=four
+// four.next=five
 
-//     let arr = []
+// let headd=first
+// let curent=headd
+// let arr=[]
 
-//     while (current !== null) {
-//         arr.push(current.data)
-//         current = current.next
-//     }
-
-//     while (arr.length > 0) {
-//         console.log(arr.pop());
-
-//     }
-
+// while (curent!==null) {
+//     arr.push(curent.value)
+//     curent=curent.next
 // }
 
-// revrselist(current)
+// for (let i = arr.length-1; i>=0; i--) {
+// console.log(arr[i]);
+// }
 
 
 
