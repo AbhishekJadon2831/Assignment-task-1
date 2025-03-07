d1668be2115d7383c8f83b7b80e0f6aef1e17856
AbhishekJadon2831 <abhishekjadon616@gmail.com>
2025-03-07 10:33:33 +0530

    Add files via upload

--- Assinment task/Assignment-task-1/index1.js
+++ Assinment task/Assignment-task-1/index1.js
@@ -2254,42 +2254,42 @@
 //Assignment: Mastering Linked Lists               
 
 
-// class node {
-//     constructor(data) {                          // Q-1
-//         this.data = data
-//         this.next = null
-//     }
-// }
+class node {
+    constructor(data) {                          // Q-1
+        this.data = data
+        this.next = null
+    }
+}
 
-// let a = new node(10)
-// let b = new node(20)
-// let c = new node(30)
-// let d = new node(40)
-// let e = new node(50)
+let a = new node(10)
+let b = new node(20)
+let c = new node(30)
+let d = new node(40)
+let e = new node(50)
 
-// a.next = b
-// b.next = c
-// c.next = d
-// d.next = e
+a.next = b
+b.next = c
+c.next = d
+d.next = e
 
-// let head = a
-// let current = head
-// let count = 0
+let head = a
+let current = head
+let count = 0
 
-// while (current !== null) {
-//     count++;
-//     current = current.next
-// }
+while (current !== null) {
+    count++;
+    current = current.next
+}
 
-// let res = Math.floor(count / 2)
+let res = Math.floor(count / 2)
 
-// current = head
+current = head
 
-// for (let i = 0; i < res; i++) {
-//     current = current.next
-// }
+for (let i = 0; i < res; i++) {
+    current = current.next
+}
 
-// console.log(current.data);
+console.log(current.data);
 
 
 
